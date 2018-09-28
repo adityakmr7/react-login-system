ce4bd845a03832833cbac5dbeb5edd37eb638a9d
aditya <adityakmr9672@gmail.com>
2018-09-28 15:35:39 +0530

    login/logout bug fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import decode from 'jwt-decode';
 import './index.css';
 import App from './App';
+import {userLoggedIn} from './actions/auth';
 import { BrowserRouter, Route } from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
@@ -16,6 +18,15 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+if(localStorage.loginToken) {
+  const payload = decode(localStorage.loginToken);
+  const user = {
+    token: localStorage.loginToken,
+    email: payload.email,
+  };
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
 <BrowserRouter>
